db73ebf9db68f46a86633ca86c0007713eceebd6
waveyan <593310640@qq.com>
2017-06-09 00:32:59 +0800

    ListView and DetailView

--- myblog/blogapp/urls.py
+++ myblog/blogapp/urls.py
@@ -6,6 +6,7 @@ urlpatterns=[
     #url(r'^detail/(?P<pk>[0-9]+)/(?P<id>[0-9]+)/$',views.detail,name='detail'),这样可以传递多个参数
     url(r'^detail/(?P<pk>[0-9]+)/$',views.detail,name='detail'),
     url(r'^posts/$',views.posts,name='posts'),
-    url(r'^category/(?P<pk>[0-9]+)/$',views.get_posts_with_category,name='get_post_with_category'),
+    #第二个参数必须为函数
+    url(r'^category/(?P<pk>[0-9]+)/$',views.CategoryView.as_view(),name='get_post_with_category'),
 
 ]
--- myblog/blogapp/views.py
+++ myblog/blogapp/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render,get_object_or_404
 from .models import Post,Category
 from comments.forms import CommentForm
 from django.db.models.aggregates import Count
+from django.views.generic import ListView,DetailView
 
 def index(request):
     categories=Category.objects.annotate(post_num=Count('post')).all()
@@ -24,9 +25,44 @@ def detail(request,pk):
     context={'post':post,'categories':categories,'comments':comments,'commentform':commentform,'posts':posts}
     return render(request,'blogapp/detail.html',context)
 
+#功能与detail一致
+class DetailView(DetailView):
+    model=Post
+    template_name='blogapp/detail.html'
+    context_object_name='post'
+
+    #重写,这是DetailView最后调用的,会调用其他DetailView的内置函数
+    def get(self,request,*args,**kwargs):
+        #调用父类的get DetailView才会根据url的pk,调用get_object找到相应的post,即self.object
+        response=super().get(self,request,*arg,**kwargs)
+        #阅读量
+        self.object.increase_read()
+        #返回一个HttpResponse
+        return response
+
+    #def get_object
+
+    #重写，也会自动被get调用
+    def get_context_data(self,**kwargs):
+        context=super().get_context_data(**kwargs)
+        posts=Post.objects.all()[:6]
+        categories=Category.objects.all()
+        comments=post.comment_set.all()
+        commentform=CommentForm()
+        #post自动了
+        context.update({
+            'categories':categories,
+            'comments':comments,
+            'commentform':commentform,
+            'posts':posts,
+        })
+
+
+
+
+
 def posts(request):
     posts=Post.objects.annotate(comment_num=Count('comment')).all()
-    context={'posts':posts}
     categories=Category.objects.annotate(post_num=Count('post')).all()
     context={'categories':categories,'posts':posts}
     return render(request,'blogapp/posts.html',context)
@@ -36,6 +72,50 @@ def get_posts_with_category(request,pk):
     categories=Category.objects.annotate(post_num=Count('post')).all()
     context={'categories':categories,'posts':category.post_set.all()}
     return render(request,'blogapp/posts.html',context)
+
+#使用类视图,功能与get_post_with_category一致
+class CategoryView(ListView):
+    #需要获取post的列表
+    model=Post
+    template_name='blogapp/posts.html'
+    #post列表在context的name
+    context_object_name='posts'
+
+    #重写get_queryset,表示如何获取这个posts列表
+    def get_queryset(self):
+        #获取url上的pk
+        category=get_object_or_404(Category,pk=self.kwargs.get('pk'))
+        #return super().get_queryset().filter(category=category)
+        return Post.objects.annotate(comment_num=Count('comment')).filter(category=category)
+
+    #重写get_contxt_data
+    def get_context_data(self,**kwargs):
+        #先获取posts list，即调用了get_queryset
+        #post list交由ListView获取了，我自己获取categories
+        context=super().get_context_data(**kwargs)
+        categories=Category.objects.annotate(post_num=Count('post')).all()
+        context.update({'categories':categories})
+        return context
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
